bd335971268e14ffbbe2385725b150c86a667529
Sugato Choudhury <prinesug@gmail.com>
2018-10-26 00:49:17 +0530

    Day 2

---
+++ app/src/main/java/com/example/princ/ssrchemicals/Material_Inspection_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.princ.ssrchemicals
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Material_Inspection_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_material__inspection)
+    }
+}
