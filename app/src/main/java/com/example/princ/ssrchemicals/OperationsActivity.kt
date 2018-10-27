64312f622c5f6f4e9d1a09677c4b706ffd5a64fb
Sugato Choudhury <prinesug@gmail.com>
2018-10-27 11:10:10 +0530

    Day 4

--- app/src/main/java/com/example/princ/ssrchemicals/OperationsActivity.kt
+++ app/src/main/java/com/example/princ/ssrchemicals/OperationsActivity.kt
@@ -1,12 +1,39 @@
 package com.example.princ.ssrchemicals
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_operations.*
 
 class OperationsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_operations)
+
+    button1.setOnClickListener()
+    {
+        val intent = Intent(this,MaterialInspectionActivity::class.java)
+        startActivity(intent)
+    }
+
+    button2.setOnClickListener()
+    {
+        val intent = Intent(this,MaterialPickupActivity::class.java)
+        startActivity(intent)
+    }
+
+    button3.setOnClickListener()
+    {
+        val intent = Intent(this,CustomerVisitActivity::class.java)
+        startActivity(intent)
+    }
+
+        btnLogout.setOnClickListener()
+        {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
